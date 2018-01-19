18679c7c2ac69b993de1d52d1c2d75560cf4f235
littlesqx <littlesqx@gmail.com>
2018-01-20 00:14:09 +0800

    Move aside to a component

---
+++ src/components/LeftAside.vue
@@ -0,0 +1,98 @@
+<template>
+  <el-aside width="386px" class="setting">
+            <el-tabs type="border-card">
+            <el-tab-pane label="基础信息">
+                <el-form ref="form" :model="form" label-width="100px">
+                    <div class="topic">
+                        <span>基础信息</span>
+                    </div>
+                    <el-form-item :label="item.label" v-for="(item, index) in profile" :key="index">
+                        <el-input v-model="item.value"></el-input>
+                    </el-form-item>
+                </el-form>
+            </el-tab-pane>
+            <el-tab-pane label="项目经历">
+                <div class="topic">
+                    <span>项目经历</span>
+                    <div class="option">
+                        <el-button size="mini" plain icon="el-icon-plus" @click="addExperience">新增经历</el-button>
+                     </div>
+                </div>
+                <el-form v-for="(item, index) in experiences" :key="index" label-width="100px">
+                  <el-form-item label="项目 / 公司">
+                    <el-input v-model="item.company"></el-input>
+                  </el-form-item>
+                  <el-form-item label="职位">
+                    <el-input v-model="item.position"></el-input>
+                  </el-form-item>
+                  <el-form-item label="时间">
+                    <el-input v-model="item.date"></el-input>
+                  </el-form-item>
+                  <el-form-item label="描述">
+                    <el-input type="textarea" v-model="item.description" :rows="3"></el-input>
+                  </el-form-item>
+                </el-form>
+            </el-tab-pane>
+            <el-tab-pane label="技能清单">
+                <div class="topic">
+                    <span>技能清单</span>
+                    <div class="option">
+                      <el-popover
+                        ref="popover"
+                        placement="right"
+                        trigger="click">
+                        <el-form :inline="true">
+                          <el-form-item>
+                            <el-input v-model="skill"></el-input>
+                          </el-form-item>
+                          <el-form-item>
+                            <el-button plain size="small" @click="addSkill">新增</el-button>
+                          </el-form-item>
+                        </el-form>
+                      </el-popover>
+                      <el-button size="mini" plain icon="el-icon-plus" v-popover:popover>新增技能</el-button>
+                     </div>
+                </div>
+                <el-form ref="form" :model="form" label-width="100px">
+                    <el-form-item label="参考技能">
+                    <el-checkbox-group v-model="form.skills">
+                        <el-checkbox :label="item" name="type" v-for="(item, index) in skills" :key="index">
+                        </el-checkbox>
+                    </el-checkbox-group>
+                </el-form-item>
+                </el-form>
+            </el-tab-pane>
+            <el-tab-pane label="主题设置">主题设置</el-tab-pane>
+            </el-tabs>
+        </el-aside>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: Array,
+    experiences: Array,
+    skills: Array,
+    form: Object
+  },
+  data () {
+    return {
+      skill: ''
+    }
+  },
+  methods: {
+    addExperience () {
+      this.experiences.push({
+        company: '',
+        position: '',
+        date: '',
+        description: ''
+      })
+    },
+    addSkill () {
+      this.skills.push(this.skill)
+      this.skill = ''
+    }
+  }
+}
+</script>
--- src/views/Base.vue
+++ src/views/Base.vue
@@ -31,73 +31,7 @@
         <head-menu></head-menu>
     </el-header>
     <el-container class="main">
-        <el-aside width="400px" class="setting">
-            <el-tabs type="border-card">
-            <el-tab-pane label="基础信息">
-                <el-form ref="form" :model="form" label-width="100px">
-                    <div class="topic">
-                        <span>基础信息</span>
-                    </div>
-                    <el-form-item :label="item.label" v-for="(item, index) in profile" :key="index">
-                        <el-input v-model="item.value"></el-input>
-                    </el-form-item>
-                </el-form>
-            </el-tab-pane>
-            <el-tab-pane label="项目经历">
-                <div class="topic">
-                    <span>项目经历</span>
-                    <div class="option">
-                        <el-button size="mini" plain icon="el-icon-plus" @click="addExperience">新增经历</el-button>
-                     </div>
-                </div>
-                <el-form v-for="(item, index) in experiences" :key="index" label-width="100px">
-                  <el-form-item label="项目 / 公司">
-                    <el-input v-model="item.company"></el-input>
-                  </el-form-item>
-                  <el-form-item label="职位">
-                    <el-input v-model="item.position"></el-input>
-                  </el-form-item>
-                  <el-form-item label="时间">
-                    <el-input v-model="item.date"></el-input>
-                  </el-form-item>
-                  <el-form-item label="描述">
-                    <el-input type="textarea" v-model="item.description" :rows="3"></el-input>
-                  </el-form-item>
-                </el-form>
-            </el-tab-pane>
-            <el-tab-pane label="技能清单">
-                <div class="topic">
-                    <span>技能清单</span>
-                    <div class="option">
-                      <el-popover
-                        ref="popover"
-                        placement="right"
-                        width="400"
-                        trigger="click">
-                        <el-form :inline="true">
-                          <el-form-item>
-                            <el-input v-model="skill"></el-input>
-                          </el-form-item>
-                          <el-form-item>
-                            <el-button plain size="small" @click="addSkill">新增</el-button>
-                          </el-form-item>
-                        </el-form>
-                      </el-popover>
-                      <el-button size="mini" plain icon="el-icon-plus" v-popover:popover>新增技能</el-button>
-                     </div>
-                </div>
-                <el-form ref="form" :model="form" label-width="100px">
-                    <el-form-item label="参考技能">
-                    <el-checkbox-group v-model="form.skills">
-                        <el-checkbox :label="item" name="type" v-for="(item, index) in skills" :key="index">
-                        </el-checkbox>
-                    </el-checkbox-group>
-                </el-form-item>
-                </el-form>
-            </el-tab-pane>
-            <el-tab-pane label="主题设置">主题设置</el-tab-pane>
-            </el-tabs>
-        </el-aside>
+        <left-aside :profile="profile" :experiences="experiences" :skills="skills" :form="form"></left-aside>
         <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -106,6 +40,7 @@
 <script>
 
 import HeadMenu from '../components/HeadMenu.vue'
+import LeftAside from '../components/LeftAside.vue'
 import config from '../config'
 
 export default {
@@ -118,26 +53,14 @@ export default {
         profile: config.profile,
         experiences: config.experiences,
         skills: []
-      },
-      skill: ''
+      }
     }
   },
   methods: {
-    addExperience () {
-      config.experiences.push({
-        company: '',
-        position: '',
-        date: '',
-        description: ''
-      })
-    },
-    addSkill () {
-      config.skills.push(this.skill)
-      this.skill = ''
-    }
+    // eslint-disable-next-line
   },
   components: {
-    HeadMenu
+    HeadMenu, LeftAside
   },
   created () {
     // eslint-disable-next-line
