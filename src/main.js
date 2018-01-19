5131c40db8438c8fcbc926d4e56c471653389634
littlesqx <littlesqx@gmail.com>
2018-01-19 20:05:08 +0800

    Add ElementUI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,75 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import {
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  InputNumber,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Popover,
+  Tooltip,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Slider,
+  Icon,
+  Row,
+  Col,
+  Badge,
+  Card,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Loading
+} from 'element-ui'
+
+Vue.use(Dropdown)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Popover)
+Vue.use(Tooltip)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Slider)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Badge)
+Vue.use(Card)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+
+Vue.use(Loading.directive)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
